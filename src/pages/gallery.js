7047a9d83a1691fa9f70dcd43b7944bfaee78c2f
BGrant-git <bengranthome@gmail.com>
2020-12-20 22:38:24 +0000

    added gatsby image gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,7 +1,60 @@
+import { graphql } from "gatsby"
 import React from "react"
+import Grid from "@material-ui/core/Grid"
+import styled from "styled-components"
 
-const GalleryPage = () => {
-  return <div>gallery</div>
+import Gallery from "@browniebroke/gatsby-image-gallery"
+import "@browniebroke/gatsby-image-gallery/dist/style.css"
+
+import Layout from "../components/layout"
+
+const GalleryContainer = styled.div`
+  width: 100%;
+  margin: auto;
+`
+
+const Title = styled.h1`
+  text-align: center;
+  padding: 20px;
+  font-size: 45px;
+`
+
+const GalleryPage = ({ data }) => {
+  const images = data.allFile.edges.map(({ node }) => node.childImageSharp)
+  // `images` is an array of objects with `thumb` and `full`
+  return (
+    <Layout>
+      <Grid container>
+        <Grid item xs={false} sm={2} />
+        <Grid item xs={12} sm={8}>
+          <GalleryContainer>
+            <Title>Gallery</Title>
+            <Gallery images={images} />
+          </GalleryContainer>
+        </Grid>
+        <Grid item xs={false} sm={2} />
+      </Grid>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  query ImagesForGallery {
+    allFile {
+      edges {
+        node {
+          childImageSharp {
+            thumb: fluid(maxWidth: 300, maxHeight: 300) {
+              ...GatsbyImageSharpFluid
+            }
+            full: fluid(maxWidth: 1024) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default GalleryPage
