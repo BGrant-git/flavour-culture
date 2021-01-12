3f67eeb4c2ef0d84cc3aa17820b0da2097edc969
BGrant-git <bengranthome@gmail.com>
2021-01-12 16:25:41 +0000

    build ready for the lads

--- src/components/contactComponent.js
+++ src/components/contactComponent.js
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import Grid from "@material-ui/core/Grid"
+import { navigate } from "gatsby"
 
 const ContactForm = styled.form`
   width: 100%;
@@ -8,6 +9,7 @@ const ContactForm = styled.form`
   display: flex;
   align-items: center;
   color: white;
+  font-family: inherit;
 `
 
 const GridSection = styled(Grid)`
@@ -23,12 +25,15 @@ const Input = styled.input`
   height: 40px;
   width: 100%;
   padding: 5px;
+  font-family: inherit;
+  font-size: 18px;
 `
 
 const InputMessage = styled.textarea`
   height: 150px;
   width: 100%;
   padding: 5px;
+  font-family: inherit;
 `
 
 const Header = styled.p`
@@ -38,16 +43,48 @@ const Header = styled.p`
 const Submit = styled.button`
   margin: 5px 5px 5px 0;
   padding: 10px;
-  font-family: inherit;
 `
 
-const ContactComponent = () => {
+// This function encodes the captured form data in the format that Netlify's backend requires
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
+const ContactComponent = props => {
+  const [name, setName] = useState("")
+
+  const handleChange = e => {
+    setName({ ...name, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = event => {
+    // Prevent the default onSubmit behavior
+    event.preventDefault()
+    // POST the encoded form with the content-type header that's required for a text submission
+    // Note that the header will be different for POSTing a file
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": event.target.getAttribute("name"),
+        ...name,
+      }),
+    })
+      // On success, redirect to the custom success page using Gatsby's `navigate` helper function
+      .then(() => navigate("/"))
+      // On error, show the error in an alert
+      .catch(error => alert(error))
+  }
+
   return (
     <ContactForm
       method="post"
       netlify-honeypot="bot-field"
       data-netlify="true"
       name="contact-box"
+      onSubmit={handleSubmit}
     >
       <input type="hidden" name="form-name" value="contact" />
       <Grid container>
@@ -55,7 +92,7 @@ const ContactComponent = () => {
         <GridSection item xs={12} sm={5}>
           <Title>Contact Us</Title>
           <Header>Name:</Header>
-          <Input type="text" name="name" />
+          <Input type="text" name="name" onChange={handleChange} />
           <Header>Email Address:</Header>
           <Input type="email" name="email" />
         </GridSection>
--- src/components/homepage/home.js
+++ src/components/homepage/home.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { useBreakpoint } from 'gatsby-plugin-breakpoints'
-import Grid from '@material-ui/core/Grid'
+import React from "react"
+import { useBreakpoint } from "gatsby-plugin-breakpoints"
+import Grid from "@material-ui/core/Grid"
 
-import HomeMobile from './homeMobile'
-import HomeDesktop from './homeDesktop'
+import HomeMobile from "./homeMobile"
+import HomeDesktop from "./homeDesktop"
+import ContactComponent from "../contactComponent"
 
 const Home = () => {
-	const breakpoints = useBreakpoint()
+  const breakpoints = useBreakpoint()
 
-	return (
-		<Grid container>
-			<Grid item xs={12}>
-				{breakpoints.sm ? 
-					<HomeMobile /> 
-					:
-					<HomeDesktop />	
-				}
-			</Grid>
-		</Grid>
-	)
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        {breakpoints.sm ? <HomeMobile /> : <HomeDesktop />}
+      </Grid>
+      <Grid item xs={12}>
+        <ContactComponent />
+      </Grid>
+    </Grid>
+  )
 }
- 
+
 export default Home
