4900a942bc82cdd5dd2c1a524846ba90399e54a9
BGrant-git <bengranthome@gmail.com>
2021-01-25 12:29:30 +0000

    merge re-populated file

---
+++ src/components/homepage/galleryComponentDesktop.js
@@ -0,0 +1,33 @@
+import React from "react"
+import ImageGallery from "react-image-gallery"
+
+const ImageGalleryComponent = () => {
+  const images = [
+    {
+      original: require("../../../content/gallery/Indian/3.jpg"),
+      thumbnail: "",
+    },
+    {
+      original: require("../../../content/gallery/Indian/4.jpg"),
+      thumbnail: "",
+    },
+    {
+      original: require("../../../content/gallery/Brathaus/PXL_20201125_124046901.PORTRAIT~2.jpg"),
+      thumbnail: "",
+    },
+  ]
+
+  return (
+    <ImageGallery
+      items={images}
+      showThumbnails={false}
+      showFullscreenButton={false}
+      autoPlay={true}
+      showPlayButton={false}
+      showNav={false}
+      slideInterval="5000"
+    />
+  )
+}
+
+export default ImageGalleryComponent
