587100fc2910303a189b7385ad0a122949cd2b23
BGrant-git <bengranthome@gmail.com>
2021-01-26 19:06:44 +0000

    added ykn to .env

--- src/components/homepage/map.js
+++ src/components/homepage/map.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api"
 
+const API_KEY = process.env.API_KEY
+
 const MapComponent = props => {
   return (
-    <LoadScript googleMapsApiKey="">
+    <LoadScript googleMapsApiKey={API_KEY}>
       <GoogleMap
         mapContainerStyle={props.mapStyles}
         zoom={16}
