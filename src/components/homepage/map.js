5369de45cf02dc6ad3ef0c6e08c69374fe6bbe94
BGrant-git <bengranthome@gmail.com>
2021-01-31 16:04:25 +0000

    map markers sorted

--- src/components/homepage/map.js
+++ src/components/homepage/map.js
@@ -2,14 +2,33 @@ import React from "react"
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api"
 
 const MapComponent = props => {
+  const markerLocations = [
+    [
+      {
+        lat: 51.50799771795835,
+        lng: -0.27228767069153964,
+        loc: "B",
+      },
+
+      {
+        lat: 51.48258,
+        lng: -0.20022,
+        loc: "A",
+      },
+    ],
+  ]
+
   return (
     <LoadScript googleMapsApiKey={process.env.GATSBY_GOOGLE_API_KEY}>
       <GoogleMap
         mapContainerStyle={props.mapStyles}
         zoom={props.zoom}
         center={props.center}
+        clickableIcons={false}
       >
-        <Marker position={props.center} />
+        {markerLocations[0].map((item, i) => (
+          <Marker key={i} position={item} label={item.loc} />
+        ))}
       </GoogleMap>
     </LoadScript>
   )
