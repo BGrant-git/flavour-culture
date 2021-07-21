c759be2a06fb18554f41d052d781d9bf723a88da
BGrant-git <bengranthome@gmail.com>
2021-07-21 13:57:23 +0100

    shop page added

--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -1,20 +1,62 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 
 import Layout from "../../components/layout"
 
-const Container = styled.div`
-  text-align: center;
-  height: 600px;
-  padding-top: 30px;
+import mandala from "../../images/henna/mandala2.png"
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: ${props => props.height}px;
 `
 
+const StyledImg = styled.img`
+  width: 400px;
+  height: 400px;
+
+  @media (max-width: 1000px) {
+    width: 250px;
+    height: 250px;
+  }
+`
+
+const TextWrapper = styled.div`
+  margin: 0 auto 10px;
+  max-width: 300px;
+  text-align: center;
+`
+
+const getHeight = () =>
+  window !== "undefined" ? window.innerHeight - 155 : null
+
 const Shop = () => {
+  const [windowHeight, setWindowHeight] = useState(getHeight())
+
+  useEffect(() => {
+    let timeoutId = null
+    const resizeListener = () => {
+      clearTimeout(timeoutId)
+      timeoutId = setTimeout(() => setWindowHeight(getHeight()), 150)
+    }
+
+    window.addEventListener("resize", resizeListener)
+
+    return () => {
+      window.removeEventListener("resize", resizeListener)
+    }
+  }, [])
+
   return (
     <Layout>
-      <Container>
-        <h2>Shop coming soon!</h2>
-      </Container>
+      <ContentWrapper height={windowHeight}>
+        <TextWrapper>
+          <h1>Flavour Culture Shop coming soon!</h1>
+        </TextWrapper>
+        <StyledImg src={mandala} alt="" />
+      </ContentWrapper>
     </Layout>
   )
 }
